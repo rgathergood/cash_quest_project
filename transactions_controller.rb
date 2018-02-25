77685ead0341a4170a62c2e4080d625a48869bbd
rgathergood <rgathergood@gmail.com>
2018-02-25 09:06:21 +0000

    Update transaction function now working

--- transactions_controller.rb
+++ transactions_controller.rb
@@ -6,5 +6,36 @@ require_relative( 'models/tag.rb')
 
 get '/cash-quest' do
   @transactions = Transaction.all()
+  @total = Transaction.total()
   erb( :index )
+end
+
+get '/cash-quest/new' do
+  @merchants = Merchant.all()
+  @tags = Tag.all()
+  erb(:new)
+end
+
+post '/cash-quest' do
+  @transaction = Transaction.new(params)
+  @transaction.save()
+  erb(:create)
+end
+
+get '/cash-quest/:id' do
+  @transaction = Transaction.find(params[:id].to_i)
+  erb(:show)
+end
+
+get '/cash-quest/:id/edit' do
+  @merchants = Merchant.all()
+  @tags = Tag.all()
+  @transaction = Transaction.find(params[:id].to_i)
+  erb(:edit)
+end
+
+post '/cash-quest/:id' do
+  transaction = Transaction.new(params)
+  transaction.update()
+  redirect to "/cash-quest/#{params['id']}"
 end
